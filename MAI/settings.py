8b6e288c194b9f55d04034ba18aaaee7ac9ae5e5
Marcentella <zaindotea@gmail.com>
2025-02-07 20:36:48 -0300

    collected static

--- MAI/settings.py
+++ MAI/settings.py
@@ -77,10 +77,11 @@ WSGI_APPLICATION = 'MAI.wsgi.application'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'MAIapp', 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
