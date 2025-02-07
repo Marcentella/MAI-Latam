7e2a5de533f5461a74941292a010fac37e0dc28c
Marcentella <zaindotea@gmail.com>
2025-02-07 20:10:19 -0300

    added sqlite3

--- MAI/settings.py
+++ MAI/settings.py
@@ -72,12 +72,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MAI.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MAIapp', 'static')]
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'MAIApp', 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
